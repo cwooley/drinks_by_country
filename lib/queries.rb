9079b829a086415625d0f9cf94ee6dda4ca54b7e
github username <github email address>
2017-06-21 09:39:18 -0400

    Added countries class, this is where the majority of the unit conversion math will take place

--- lib/queries.rb
+++ lib/queries.rb
@@ -1,10 +1,21 @@
 require_relative '../enviornment.rb'
 class Queries
-  # AVG POOL SIZE 15x 30 16,875 galleons volume = 63879 liters
-  # Lake George has 550 billion gallons of water maybe we can compare by that too depending on quantity
+
   @@AVGPOOLSIZE = 63879
-  def most_beer
-    DrinksByCountry.most_beer
+
+  def most_beer_by_volume
+    sql = "SELECT * FROM country_pop
+    JOIN drinks_by_country ON country_pop.country_name = drinks_by_country.country
+    ORDER BY (country_pop.population * drinks_by_country.beer_servings) DESC LIMIT 1"
+    attr_hash = DrinksByCountry.db.execute(sql)[0]
+    most_beer_country = Country.new(attr_hash)
+    puts "The country that drinks the most beer is #{most_beer_country.name}"
+    puts "They drink #{most_beer_country.liters_of_beer} liters of beer every year"
+  end
+
+  def pop_of_china
+    sql = "SELECT * FROM country_pop WHERE country_name = 'China'"
+    DrinksByCountry.db.execute(sql)
   end
 
   def pools_of_beer_by_country(country)
@@ -15,7 +26,7 @@ class Queries
 
   end
 
-  def lake_georges_of_beer_by_country(country)
+  def central_park_lakes_of_beer_by_country(country)
 
   end
 
@@ -27,7 +38,7 @@ class Queries
 
   end
 
-  def lake_georges_of_wine_by_country(country)
+  def central_park_lakes_of_wine_by_country(country)
 
   end
 
@@ -39,7 +50,7 @@ class Queries
 
   end
 
-  def lake_georges_of_spirits_by_country(country)
+  def central_park_lakes_of_spirits_by_country(country)
 
   end
 
@@ -49,4 +60,5 @@ end
 
 query = Queries.new
 
-puts query.most_beer
+query.most_beer_by_volume
+# puts query.pop_of_china
---
+++ models/country.rb
@@ -0,0 +1,28 @@
+class Country
+  attr_accessor :name, :beer_servings, :wine_servings, :spirit_servings, :population
+
+  # AVG POOL SIZE 15x 30 16,875 galleons volume = 63879 liters
+  # Lake George has 550 billion gallons of water maybe we can compare by that too depending on quantity
+  # central park resovoir 3800000000 liters
+  
+  def initialize(attr_hash)
+    @name = attr_hash["country_name"]
+    @beer_servings = attr_hash["beer_servings"]
+    @wine_servings = attr_hash["wine_servings"]
+    @spirit_servings = attr_hash["spirit_servings"]
+    @population = attr_hash["population"]
+  end
+
+  def liters_of_beer
+    self.beer_servings * self.population * 12 / 33.814
+  end
+
+  def pools_of_beer
+
+  end
+
+  def central_park_lakes_of_beer
+
+  end
+
+end
